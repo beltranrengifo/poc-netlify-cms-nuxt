ef41b18de95c8a7b910c35eb2da11bf2fda0ee51
Beltrán Rengifo <b.rengifo@docline.es>
2021-05-07 17:02:18 +0200

    feat: add Netlify Identity Widget

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,15 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      ],
+    }
+  },
+})
 </script>
 
 <style>
